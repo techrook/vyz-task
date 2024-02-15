0a8f88523f553dcba0ca73fa4917103aa8912e65
techrook <mondayitohowo3@gmail.com>
2024-02-15 17:09:19 +0100

    updated payment

--- src/controllers/payment.contrller.js
+++ src/controllers/payment.contrller.js
@@ -6,10 +6,9 @@ const payment = async(req,res)=>{
 
     const sig = req.headers['stripe-signature'];
     let event;
-    console.log( process.env.STRIPE_WEBHOOK_SECRET)
-    console.log( sig)
+    const rawBody = JSON.stringify(req.body);
     try {
-        event = await  stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+        event = await  stripe.webhooks.constructEvent(rawBody, sig, process.env.STRIPE_WEBHOOK_SECRET);
     } catch (err) {
         console.error('Error verifying webhook signature:', err.message);
         return res.status(400).send(`Webhook Error: ${err.message}`);
@@ -17,8 +16,7 @@ const payment = async(req,res)=>{
 
     if (event.type === 'payment_intent.succeeded') {
         const paymentIntent = event.data.object;
-        console.log(req)
-        //await User.findByIdAndUpdate()
+        console.log(paymentIntent)
     }
 }
 
